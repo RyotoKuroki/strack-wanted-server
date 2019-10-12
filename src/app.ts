395e793131a27c8f117f9501500fdb53ec228596
RyotoKuroki <kurohoge@gmail.com>
2019-10-13 03:24:35 +0900

    BodyParser警告対応 Azureデプロイ時のログ警告対応

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
-import Logger from './app.logger/Logger';
+// import Logger from './app.logger/Logger';
 
 const app = express();
 
-app.use(bodyParser({
+app.use(bodyParser.urlencoded({
     limit: '5mb',
     extended: true,
 }));
 app.use('/', routes);
 
-Logger.Test();
+// Logger.Test();
 
 export default app;
