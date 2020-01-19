92450a72ccde10ad83e005b72f6c2a94d4c3c963
RyotoKuroki <kurohoge@gmail.com>
2020-01-19 00:21:31 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ import bodyParser from 'body-parser';
 import routes from './routes';
 import DataStore from './app.infras/datastores/datastore.mysql';
 
-DataStore.Init();
-
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(bodyParser.urlencoded({
-    limit: '3mb',
-    extended: true,
-}));
-app.use('/', routes);
-app.listen(port);
-
-// for debug.
-console.log(`http://localhost:${port}`);
+DataStore.Init()
+.then(() => {
+    const app = express();
+    const port = process.env.PORT || 3000;
+    app.use(bodyParser.urlencoded({
+        limit: '3mb',
+        extended: true,
+    }));
+    app.use('/', routes);
+    app.listen(port);
+    
+    // for debug.
+    console.log(`http://localhost:${port}`);
+})
+.catch(error => {
+    console.log(`DB接続エラー：${JSON.stringify(error)}`);
+});
